5ac3530b810f806dacbaf7fe8f0ab006e3b9c6c6
radzikkos <lukaszrad99@wp.pl>
2021-01-13 19:29:14 +0100

    Better to save

--- Aplikacja Spa/index.js
+++ Aplikacja Spa/index.js
@@ -23,19 +23,42 @@ app.get('/items', db.getItems)
 app.get('/prices', db.getPrices)
 app.get('/rooms', db.getRooms)
 app.get('/seances', db.getSeances)
+app.get('/itemUsedInSeance', db.getItemUsedInSeance)
+app.get('/salaries', db.getSalaries)
+app.get('/employees', db.getEmployees)
+app.get('/employees/:id', db.getEmployeeById)
+app.get('/courses', db.getCourses)
+app.get('/courses/:name', db.getCourseByName)
+app.get('/coursesAndSeances', db.getCoursesAndSeances)
+app.get('/coursesAndSeances/:name', db.getCourseAndSeancesByCourseName)
 
 app.post('/items', db.createItem)
 app.post('/prices', db.createPrice)
 app.post('/rooms', db.createRoom)
 app.post('/seances', db.createSeance)
+app.post('/itemUsedInSeance', db.createItemUsedInSeance)
+app.post('/salaries', db.createSalary)
+app.post('/employees', db.createEmployee)
+app.post('/courses', db.createCourse)
+app.post('/coursesAndSeances', db.createCourseAndSeance)
+
 
 app.put('/items/:name', db.updateItem)
 app.put('/seances/:type', db.updateSeance)
+app.put('/itemUsedInSeance/:seance_name/:item_name', db.updateItemUsedInSeance)
+app.put('/salaries/:previous_salary', db.updateSalary)
+app.put('/employees/:name/:surname/:workstand/:salary', db.updateEmployee)
+app.put('/courses/:name', db.updateCourse)
 
 app.delete('/items/:name', db.deleteItem)
 app.delete('/prices/:price', db.deletePrice)
 app.delete('/rooms/:nr', db.deleteRoom)
 app.delete('/seances/:type', db.deleteSeance)
+app.delete('/itemUsedInSeance/:seance_name/:item_name', db.deleteItemUsedInSeance)
+app.delete('/salaries/:salary', db.deleteSalary)
+app.delete('/employees/:name/:surname/:workstand/:salary', db.deleteEmployee)
+app.delete('/courses/:name', db.deleteCourse)
+
 
 app.use((req, res, next) => {
     res.status(404).send({
--- Aplikacja Spa/queries.js
+++ Aplikacja Spa/queries.js
@@ -1,15 +1,15 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'ibwbksua',
-    host: 'ziggy.db.elephantsql.com',
-    database: 'ibwbksua',
-    password: 'lyyCPZGsY_DRtnOYdRY8RdvbwxHf175w',
-})
-
-
-/* 
-CRUD for items
-*/
+        user: 'ibwbksua',
+        host: 'ziggy.db.elephantsql.com',
+        database: 'ibwbksua',
+        password: 'lyyCPZGsY_DRtnOYdRY8RdvbwxHf175w',
+    })
+    /**Poprawic trigger dla klient-kurs, by odejmowal ilosc rzeczy */
+    /*Napisac get sb by ID */
+    /* 
+    CRUD for items
+    */
 const getItems = (request, response) => {
     pool.query('SELECT * FROM rzeczy_cena', (error, results) => {
         if (error) {
@@ -33,7 +33,7 @@ const updateItem = (request, response) => {
     const name = request.params.name
     const { price, amount } = request.body[0]
 
-    pool.query('UPDATE rzecz SET cena_id = zwroc_id_ceny($1), ilosc = $2 WHERE nazwa = $3', [price, amount, name], (error, results) => {
+    pool.query('UPDATE rzecz SET cena_id = zwroc_id_ceny($1), ilosc = (ilosc + $2) WHERE nazwa = $3', [price, amount, name], (error, results) => {
         if (error) {
             return response.status(400).send(`Nie mozna zmodyfikowac ${name}`)
         }
@@ -123,7 +123,8 @@ const deleteRoom = (request, response) => {
  * CRUD for seances
  */
 const getSeances = (request, response) => {
-    pool.query('SELECT * from seans', (error, results) => {
+    //pool.query('SELECT * from seans', (error, results) => {
+    pool.query('SELECT * from widok_seansu', (error, results) => {
         if (error) {
             return response.status(400).send(`Nie mozna wypisac seansu`)
         }
@@ -163,7 +164,246 @@ const deleteSeance = (request, response) => {
     })
 }
 
+/**
+ * CRUD for item used in seance -associate table
+ */
+const getItemUsedInSeance = (request, response) => {
+    pool.query('SELECT * from rzeczy_w_seansie_widok', (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna wypisac rzeczy w seansie`)
+        }
+        response.status(200).json(results.rows)
+    })
+}
 
+const createItemUsedInSeance = (request, response) => {
+    const { seance_name, item_name, amount } = request.body[0]
+    pool.query('INSERT INTO rzeczy_wykorzystane_do_seansu(seans_id, rzecz_id, uzyta_ilosc) VALUES (zwroc_id_seansu($1),zwroc_rzecz_id($2),$3);', [seance_name, item_name, amount], (error, results) => {
+        if (error) {
+            return response.status(400).send('Nie mozna dodac rzeczy to seansu')
+        }
+        response.status(201).send(`Rzecz do seansu zostala dodana`)
+    })
+}
+
+const updateItemUsedInSeance = (request, response) => {
+    const seance_name = request.params.seance_name
+    const item_name = request.params.item_name
+    const { amount } = request.body[0]
+
+    pool.query('UPDATE rzeczy_wykorzystane_do_seansu SET  uzyta_ilosc = $1 WHERE seans_id = zwroc_id_seansu($2) and rzecz_id = zwroc_rzecz_id($3)', [amount, seance_name, item_name], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna zmodyfikowac rzeczy w seansie`)
+        }
+        response.status(200).send(`Przedmiot  w seansie zostal zmodyfikowany`)
+    })
+}
+
+const deleteItemUsedInSeance = (request, response) => {
+    const seance_name = request.params.seance_name
+    const item_name = request.params.item_name
+    pool.query('DELETE FROM rzeczy_wykorzystane_do_seansu WHERE seans_id = zwroc_id_seansu($1) and rzecz_id = zwroc_rzecz_id($2)', [seance_name, item_name], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna usunac tej rzeczy w seansie`)
+        }
+        response.status(200).send(`Rzecz zostala usunieta z seansu`)
+    })
+}
+
+/**
+ * CRUD for salaries
+ */
+const getSalaries = (request, response) => {
+    pool.query('SELECT kwota from wynagrodzenie ORDER BY kwota DESC', (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna wypisac wynagrodzenia`)
+        }
+        response.status(200).json(results.rows)
+    })
+}
+const createSalary = (request, response) => {
+    const { salary } = request.body[0]
+    pool.query('INSERT INTO wynagrodzenie(kwota) VALUES ($1)', [salary], (error, results) => {
+        if (error) {
+            return response.status(400).send('Nie mozna dodac tego wynagrodzenia')
+        }
+        response.status(201).send(`Wynagrodzenie zostalo dodane`)
+    })
+}
+
+const updateSalary = (request, response) => {
+    const previous_salary = parseInt(request.params.previous_salary)
+    const { new_salary } = request.body[0]
+
+    pool.query('UPDATE wynagrodzenie SET  kwota = $2 WHERE (wynagrodzenie_id = (select w.wynagrodzenie_id from wynagrodzenie w where w.kwota = $1));', [previous_salary, new_salary], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna zmodyfikowac wynagrodzenia`)
+        }
+        response.status(200).send(`Wynagrodzenie zmodyfikowane`)
+    })
+}
+
+const deleteSalary = (request, response) => {
+        const salary = request.params.salary
+        pool.query('DELETE FROM wynagrodzenie WHERE kwota = $1', [salary], (error, results) => {
+            if (error) {
+                return response.status(400).send(`Nie mozna usunac tego wynagrodzenia`)
+            }
+            response.status(200).send(`Wynagrodzenie usuniete`)
+        })
+    }
+    /*----------------------------------------------------------------------------------- */
+    /**
+     * CRUD for employees
+     */
+const getEmployees = (request, response) => {
+    pool.query('SELECT * from pracownik_wynagrodzenie ORDER BY stanowisko', (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna wypisac wynagrodzenia`)
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getEmployeeById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('SELECT * from pracownik_wynagrodzenie where pracownik_id = $1 ORDER BY stanowisko ', [id], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna wyszukac pracownika`)
+        } else {
+            response.status(200).json(results.rows)
+        }
+
+    })
+}
+
+const createEmployee = (request, response) => {
+    const { seance_name, salary, name, surname, workstand } = request.body[0]
+    pool.query('INSERT INTO pracownik(seans_id, wynagrodzenie_id, imie, nazwisko, stanowisko) VALUES (zwroc_id_seansu($1), zwroc_wynagrodzenie_id($2),$3, $4, $5);', [seance_name, salary, name, surname, workstand], (error, results) => {
+        if (error) {
+            return response.status(400).send('Nie mozna dodac tego pracownika')
+        }
+        response.status(201).send(`Pracownik dodany`)
+    })
+}
+
+/**Mozna zmienci mu tylko wyplate - jebac */
+const updateEmployee = (request, response) => {
+    const name = request.params.name
+    const surname = request.params.surname
+    const workstand = request.params.workstand
+    const salary = parseInt(request.params.salary)
+    const new_salary = request.body[0]['new_salary']
+
+    pool.query('UPDATE pracownik set wynagrodzenie_id = zwroc_wynagrodzenie_id($5) where pracownik_id = (zwroc_id_pracownika($1, $2, $3, $4))', [name, surname, workstand, salary, new_salary], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna zmodyfikowac pracownika`)
+        }
+        response.status(200).send(`Wynagrodzenie pracownika zmienione`)
+    })
+}
+
+const deleteEmployee = (request, response) => {
+    const name = request.params.name
+    const surname = request.params.surname
+    const workstand = request.params.workstand
+    const salary = parseInt(request.params.salary)
+    pool.query('DELETE FROM pracownik WHERE pracownik_id = zwroc_id_pracownika($1, $2, $3, $4)', [name, surname, workstand, salary], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna usunac tego pracownika`)
+        }
+        response.status(200).send(`Pracownik usuniety`)
+    })
+}
+
+/**
+ * CRUD for courses
+ */
+const getCourses = (request, response) => {
+    pool.query('SELECT * from kurs_widok', (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna wypisac wynagrodzenia`)
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getCourseByName = (request, response) => {
+    const name = request.params.name
+
+    pool.query('SELECT * from kurs_widok where nazwa = $1 ', [name], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna wyszukac kursu`)
+        } else {
+            response.status(200).json(results.rows)
+        }
+
+    })
+}
+
+const createCourse = (request, response) => {
+    const { name, level } = request.body[0]
+    pool.query('INSERT INTO kurs(nazwa, poziom_luksusu) VALUES ($1, $2)', [name, level], (error, results) => {
+        if (error) {
+            return response.status(400).send('Nie mozna dodac tego kursu')
+        }
+        response.status(201).send(`Kurs dodany`)
+    })
+}
+
+const updateCourse = (request, response) => {
+    const name = request.params.name
+    const { level } = request.body[0]
+
+    pool.query('UPDATE kurs set poziom_luksusu = $1 where nazwa = $2', [level, name], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna zmodyfikowac kursu`)
+        }
+        response.status(200).send(`Level kursu zmieniony`)
+    })
+}
+
+const deleteCourse = (request, response) => {
+    const name = request.params.name
+    pool.query('DELETE FROM kurs WHERE nazwa = $1', [name], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna usunac tego kursu`)
+        }
+        response.status(200).send(`Kurs usuniety`)
+    })
+}
+
+/*
+CRUD for course and seance - associate table
+ */
+const getCoursesAndSeances = (request, response) => {
+    pool.query('SELECT * from kurs_seans_widok', (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna wypisac wynagrodzenia`)
+        }
+        response.status(200).json(results.rows)
+    })
+}
+const getCourseAndSeancesByCourseName = (request, response) => {
+    const name = request.params.name
+    pool.query('SELECT * from seanse_w_kursach where kurs = $1', [name], (error, results) => {
+        if (error) {
+            return response.status(400).send(`Nie mozna wypisac wynagrodzenia`)
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const createCourseAndSeance = (request, response) => {
+    const { course_name, seance_name, name, surname, workstand, salary } = request.body[0]
+    pool.query('INSERT INTO kurs_seans(kurs_id, seans_id, pracownik_id) VALUES (zwroc_kurs_id($1),zwroc_id_seansu($2),zwroc_id_pracownika($3, $4, $5,$6));', [course_name, seance_name, name, surname, workstand, salary], (error, results) => {
+        if (error) {
+            return response.status(400).send('Nie mozna dodac tego seansu do kursu')
+        }
+        response.status(201).send(`Seansu do kursu dodany`)
+    })
+}
 
 
 
@@ -189,4 +429,36 @@ module.exports = {
     createSeance,
     updateSeance,
     deleteSeance,
+
+    /*ItemUsedInSeance */
+    getItemUsedInSeance,
+    createItemUsedInSeance,
+    updateItemUsedInSeance,
+    deleteItemUsedInSeance,
+
+    /*Salaries */
+    getSalaries,
+    createSalary,
+    updateSalary,
+    deleteSalary,
+
+    /*Employees */
+    getEmployees,
+    getEmployeeById,
+    createEmployee,
+    updateEmployee,
+    deleteEmployee,
+
+    /*Course */
+    getCourses,
+    getCourseByName,
+    createCourse,
+    updateCourse,
+    deleteCourse,
+
+    /*CoursesAndSeances */
+    getCoursesAndSeances,
+    getCourseAndSeancesByCourseName,
+    createCourseAndSeance,
+
 }
